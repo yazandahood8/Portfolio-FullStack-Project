f859a7ce83c2fd75ab065a570d4973506912e283
yazan dahood <dahood.yazan8@gmail.com>
2025-09-01 21:48:48 +0300

    Update users.js

--- frontend/src/api/users.js
+++ frontend/src/api/users.js
@@ -2,21 +2,21 @@
 import client from './client';
 
 export const fetchUsers = async (filters = {}) => {
-  const res = await client.get('/users', { params: filters });
+  const res = await client.get('/api/v1/users', { params: filters });
   return res.data;
 };
 
 export const fetchUser = async (id) => {
-  const res = await client.get(`/users/${id}`);
+  const res = await client.get(`/api/v1/users/${id}`);
   return res.data;
 };
 
 export const updateUser = async (id, userData) => {
-  const res = await client.put(`/users/${id}`, userData);
+  const res = await client.put(`/api/v1/users/${id}`, userData);
   return res.data;
 };
 
 export const deleteUser = async (id) => {
-  const res = await client.delete(`/users/${id}`);
+  const res = await client.delete(`/api/v1/users/${id}`);
   return res.data;
 };
