a395ed5950acbc32488455b5cf8bd57b0b3ca1d1
yazan dahood <dahood.yazan8@gmail.com>
2025-09-01 21:52:31 +0300

    Update experiences.js

--- frontend/src/api/experiences.js
+++ frontend/src/api/experiences.js
@@ -2,26 +2,26 @@
 import client from './client';
 
 export const fetchExperiences = async (userId) => {
-  const res = await client.get(`/users/${userId}/experiences`);
+  const res = await client.get(`/api/v1/users/${userId}/experiences`);
   return res.data;
 };
 
 export const fetchExperience = async (userId, expId) => {
-  const res = await client.get(`/users/${userId}/experiences/${expId}`);
+  const res = await client.get(`/api/v1/users/${userId}/experiences/${expId}`);
   return res.data;
 };
 
 export const createExperience = async (userId, expData) => {
-  const res = await client.post(`/users/${userId}/experiences`, expData);
+  const res = await client.post(`/api/v1/users/${userId}/experiences`, expData);
   return res.data;
 };
 
 export const updateExperience = async (userId, expId, expData) => {
-  const res = await client.put(`/users/${userId}/experiences/${expId}`, expData);
+  const res = await client.put(`/api/v1/users/${userId}/experiences/${expId}`, expData);
   return res.data;
 };
 
 export const deleteExperience = async (userId, expId) => {
-  const res = await client.delete(`/users/${userId}/experiences/${expId}`);
+  const res = await client.delete(`/api/v1/users/${userId}/experiences/${expId}`);
   return res.data;
 };
