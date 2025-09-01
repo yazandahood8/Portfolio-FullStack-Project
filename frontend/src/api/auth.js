cbd93158556c967b27cefa54a5bdb0c44ee5d9d7
yazan dahood <dahood.yazan8@gmail.com>
2025-09-01 21:30:00 +0300

    Update auth.js

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,33 +1,22 @@
 // src/api/auth.js
 import client from './client';
-const API = 'https://portfolio-backend-ujap.onrender.com';
 
-export const register = async ({ full_name, email, password, phone, location, profile_image_url, about, bio }) => {
-  const res = await fetch(`${API}/api/v1/auth/register`, {
-    full_name,
-    email,
-    password,
-    phone,
-    location,
-    profile_image_url,
-   
-    about,
-    bio
-  });
-  return res.data;
+export const register = async (payload) => {
+  const { data } = await client.post('/api/v1/auth/register', payload);
+  return data;
 };
 
 export const login = async ({ email, password }) => {
-  const res = await client.post('${API}/api/v1//auth/login', { email, password });
-  return res.data;
+  const { data } = await client.post('/api/v1/auth/login', { email, password });
+  return data;
 };
 
 export const refreshToken = async (token) => {
-  const res = await client.post('${API}/api/v1//auth/refresh', { token });
-  return res.data;
+  const { data } = await client.post('/api/v1/auth/refresh', { token });
+  return data;
 };
 
 export const logout = async (token) => {
-  const res = await client.post('${API}/api/v1//auth/logout', { token });
-  return res.data;
+  const { data } = await client.post('/api/v1/auth/logout', { token });
+  return data;
 };
