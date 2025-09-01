2df96b2033fdf5ccf8767095223903d48f04ef98
yazan dahood <dahood.yazan8@gmail.com>
2025-09-01 22:03:15 +0300

    Update volunteerings.js

--- frontend/src/api/volunteerings.js
+++ frontend/src/api/volunteerings.js
@@ -1,13 +1,13 @@
 import client from './client';
 
 export const fetchVolunteerings = (userId) =>
-  client.get(`/users/${userId}/volunteerings`);
+  client.get(`/api/v1/users/${userId}/volunteerings`);
 
 export const createVolunteering = (userId, data) =>
-  client.post(`/users/${userId}/volunteerings`, data);
+  client.post(`/api/v1/users/${userId}/volunteerings`, data);
 
 export const updateVolunteering = (userId, volId, data) =>
-  client.put(`/users/${userId}/volunteerings/${volId}`, data);
+  client.put(`/api/v1/users/${userId}/volunteerings/${volId}`, data);
 
 export const deleteVolunteering = (userId, volId) =>
-  client.delete(`/users/${userId}/volunteerings/${volId}`);
+  client.delete(`/api/v1/users/${userId}/volunteerings/${volId}`);
